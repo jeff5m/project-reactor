ba90b3f00c08a826e66cf32edab1169f9d5b9c0c
jeff5m <jeffmatheuspereira@gmail.com>
2021-08-30 20:19:51 -0300

    class 16 - PublishOn and SubscribeOn pt 02

--- src/test/java/academy/devdojo/reactive/test/OperatorsTest.java
+++ src/test/java/academy/devdojo/reactive/test/OperatorsTest.java
@@ -48,4 +48,45 @@ class OperatorsTest {
                 .expectNext(1, 2, 3, 4)
                 .verifyComplete();
     }
+
+    @Test
+    void multipleSubscribeOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .subscribeOn(Schedulers.boundedElastic())
+                .map(i -> {
+                    log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(Schedulers.single())
+                .map(i -> {
+                    log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier
+                .create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    void multiplePublishOnSimple() {
+        Flux<Integer> flux = Flux.range(1, 4)
+                .map(i -> {
+                    log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                })
+                .publishOn(Schedulers.boundedElastic())
+                .map(i -> {
+                    log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                    return i;
+                });
+
+        StepVerifier
+                .create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
 }
